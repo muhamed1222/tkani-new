cedcfc020aa8f7f72eda206a926d8f756e5817e7
IvanZuzichev <113914925+IvanZuzichev@users.noreply.github.com>
2025-11-21 18:18:38 +0300

    Delete Downloads/app-feature-favourites-update directory

--- Downloads/app-feature-favourites-update/lib/features/places/presentation/bloc/places_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'places_bloc.dart';
-
-abstract class PlacesEvent extends Equatable {
-  const PlacesEvent();
-
-  @override
-  List<Object> get props => [];
-}
--- Downloads/app-feature-favourites-update/lib/features/routes/presentation/bloc/routes_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'routes_bloc.dart';
-
-abstract class RoutesState extends Equatable {
-  const RoutesState();  
-
-  @override
-  List<Object> get props => [];
-}
-class RoutesInitial extends RoutesState {}
