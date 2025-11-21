569d3925d76ffbe9b01a37d867f8d4d501bf96f2
IvanZuzichev <113914925+IvanZuzichev@users.noreply.github.com>
2025-11-21 18:18:49 +0300

    Delete Downloads/back-now3 directory

--- Downloads/back-now3/internal/models/route_category.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type RouteCategory struct {
-	RouteID    uint     `gorm:"primaryKey"`
-	CategoryID uint     `gorm:"primaryKey"`
-	Route      Route    `gorm:"foreignKey:RouteID"`
-	Category   Category `gorm:"foreignKey:CategoryID"`
-}
