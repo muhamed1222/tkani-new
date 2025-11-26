3337c5dd1bbe643bd59bc1057abf240093c89730
IvanZuzichev <113914925+IvanZuzichev@users.noreply.github.com>
2025-11-26 19:29:27 +0300

    Delete Downloads/back-now3 directory

--- Downloads/back-now3/internal/models/place_category.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type PlaceCategory struct {
-	PlaceID    uint     `gorm:"primaryKey"`
-	CategoryID uint     `gorm:"primaryKey"`
-	Place      Place    `gorm:"foreignKey:PlaceID"`
-	Category   Category `gorm:"foreignKey:CategoryID"`
-}
