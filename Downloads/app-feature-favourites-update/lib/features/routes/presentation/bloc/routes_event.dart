1e420ec902d59a69cb3f0acb63267a8f2d663622
IvanZuzichev <113914925+IvanZuzichev@users.noreply.github.com>
2025-12-01 11:34:23 +0300

    Delete Downloads/app-feature-favourites-update directory

--- Downloads/app-feature-favourites-update/lib/features/routes/presentation/bloc/routes_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'routes_bloc.dart';
-
-abstract class RoutesEvent extends Equatable {
-  const RoutesEvent();
-
-  @override
-  List<Object> get props => [];
-}
--- Downloads/app-feature-favourites-update/lib/shared/domain/entities/tag.dart
+++
@@ -1,8 +0,0 @@
-class Tag {
-  final int id;
-  final String name;
-  final String description;
-  final bool isActive;
-
-  const Tag({required this.id, required this.name, required this.description, required this.isActive  });
-}
