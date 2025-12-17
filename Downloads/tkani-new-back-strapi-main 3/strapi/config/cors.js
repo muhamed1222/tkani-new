952895fc8e8d812ed5c00a9811906485208957a0
IvanZuzichev <113914925+IvanZuzichev@users.noreply.github.com>
2025-12-17 13:29:11 +0300

    Delete Downloads/tkani-new-back-strapi-main 3 directory

--- Downloads/tkani-new-back-strapi-main 3/strapi/config/cors.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  enabled: true,
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
-  headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-};
